8ea32a0b7aed446e760906cc34716a134348607a
Noel Maersk <veox@wemakethings.net>
2013-11-17 03:31:25 +0200

    Don't run configure automatically.

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,3 @@ bs_dir="$(dirname $(readlink -f $0))"
 #Some versions of libtoolize don't like there being no ltmain.sh file already
 touch "${bs_dir}"/ltmain.sh
 autoreconf -fi "${bs_dir}"
-
-if test -z "$NOCONFIGURE" ; then
-	echo 'Configuring...'
-	"$bs_dir"/configure "$@"
-fi
