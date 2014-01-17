b16a05aa0eb155fd496f4874aaf601d44d86747b
Noel Maersk <veox@wemakethings.net>
2014-01-17 16:00:20 +0200

    core: lower maximum intensity to 31.
    Intensity determines the number of threads started, is an unsigned int and
    is used as (1 << intensity), so anything over 31 is meaningless.

--- miner.h
+++ miner.h
@@ -1047,8 +1047,8 @@ extern bool add_pool_details(struct pool *pool, bool live, char *url, char *user
 
 #define MIN_INTENSITY 8
 #define MIN_INTENSITY_STR "8"
-#define MAX_INTENSITY 42
-#define MAX_INTENSITY_STR "42"
+#define MAX_INTENSITY 31
+#define MAX_INTENSITY_STR "31"
 
 extern bool hotplug_mode;
 extern int hotplug_time;
