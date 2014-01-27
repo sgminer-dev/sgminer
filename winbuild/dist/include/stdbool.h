8d25b38e9b347ef72195917d1a0f50ceecec96d2
Sanjin Trošelj <sanjin.troselj@gmail.com>
2014-01-27 18:22:40 +0100

    VS2010 build: winbuild/* dist and project files.
    Cherry-picked by veox.

---
+++ winbuild/dist/include/stdbool.h
@@ -0,0 +1,21 @@
+/* stdbool.h standard header */
+
+#ifndef _STDBOOL
+#define _STDBOOL
+
+#define __bool_true_false_are_defined	1
+
+#ifndef __cplusplus
+
+#define bool	_Bool
+#define false	0
+#define true	1
+
+#endif /* __cplusplus */
+
+#endif /* _STDBOOL */
+
+/*
+ * Copyright (c) 1992-2010 by P.J. Plauger.  ALL RIGHTS RESERVED.
+ * Consult your license regarding permissions and restrictions.
+V5.30:0009 */
