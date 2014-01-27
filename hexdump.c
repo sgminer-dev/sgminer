b5a3dbeac084253c268b1d8c1ef2802980f702f0
Sanjin Trošelj <sanjin.troselj@gmail.com>
2014-01-27 18:22:40 +0100

    VS2010 build: Prepare hexdump.c
    Cherry-picked by veox.

--- hexdump.c
+++ hexdump.c
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include "miner.h"
 #define hex_print(p) applog(LOG_DEBUG, "%s", p)
 
 static char nibble[] = {
