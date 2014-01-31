95ac0c435672d4a0bd2b3a1f067c7f2e78dbedf0
Con Kolivas <kernel@kolivas.org>
2014-01-31 22:16:11 +1100

    Export the flush_queue function for use by drivers.

--- sgminer.c
+++ sgminer.c
@@ -3766,8 +3766,6 @@ int restart_wait(struct thr_info *thr, unsigned int mstime)
 
 	return rc;
 }
-	
-static void flush_queue(struct cgpu_info *cgpu);
 
 static void *restart_thread(void __maybe_unused *arg)
 {
@@ -6459,7 +6457,7 @@ struct work *take_queued_work_bymidstate(struct cgpu_info *cgpu, char *midstate,
 	return work;
 }
 
-static void flush_queue(struct cgpu_info *cgpu)
+void flush_queue(struct cgpu_info *cgpu)
 {
 	struct work *work = NULL;
 
