aca8a58f2e2605c476652ad0d501df3caaa630b1
Con Kolivas <kernel@kolivas.org>
2014-02-07 08:33:00 +1100

    Only show one decimal place if pool diff is not an integer.

--- util.c
+++ util.c
@@ -1657,7 +1657,7 @@ static bool parse_diff(struct pool *pool, json_t *val)
 		if ((double)idiff == diff)
 			applog(LOG_NOTICE, "%s difficulty changed to %d", pool->poolname ,idiff);
 		else
-			applog(LOG_NOTICE, "%s difficulty changed to %f", pool->poolname, diff);
+			applog(LOG_NOTICE, "%s difficulty changed to %.1f", pool->poolname, diff);
 	} else
 		applog(LOG_DEBUG, "%s difficulty set to %f", pool->poolname, diff);
 
