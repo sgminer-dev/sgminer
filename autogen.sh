133252175b90159d18151b004bf767d5a43812ea
Con Kolivas <kernel@kolivas.org>
2014-02-17 21:17:06 +1100

    Only run ./configure with autogen.sh if extra parameters are passed to it.

--- autogen.sh
+++ autogen.sh
@@ -4,3 +4,8 @@ bs_dir="$(dirname $(readlink -f $0))"
 #Some versions of libtoolize don't like there being no ltmain.sh file already
 touch "${bs_dir}"/ltmain.sh
 autoreconf -fi "${bs_dir}"
+
+if test -n "$1" && test -z "$NOCONFIGURE" ; then
+	echo 'Configuring...'
+	"$bs_dir"/configure "$@"
+fi
