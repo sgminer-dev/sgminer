66f508d01f851b1d92e4e388773ef4a7b4aaa745
phm <phm@inbox.com>
2014-03-05 18:00:30 +0100

    Added support for AnimeCoin

--- ocl.c
+++ ocl.c
@@ -474,6 +474,11 @@ _clState *initCl(unsigned int gpu, char *name, size_t nameSize)
 			strcpy(filename, MYRIADCOIN_GROESTL_KERNNAME".cl");
 			strcpy(binaryfilename, MYRIADCOIN_GROESTL_KERNNAME);
 			break;
+		case KL_ANIMECOIN:
+			applog(LOG_WARNING, "Kernel animecoin is experimental.");
+			strcpy(filename, ANIMECOIN_KERNNAME".cl");
+			strcpy(binaryfilename, ANIMECOIN_KERNNAME);
+			break;
 		case KL_NONE: /* Shouldn't happen */
 			break;
 	}
--- sgminer.c
+++ sgminer.c
@@ -4239,6 +4239,9 @@ void write_config(FILE *fcfg)
 				case KL_MYRIADCOIN_GROESTL:
 					fprintf(fcfg, MYRIADCOIN_GROESTL_KERNNAME);
 					break;
+				case KL_ANIMECOIN:
+					fprintf(fcfg, ANIMECOIN_KERNNAME);
+					break;
 			}
 		}
 
@@ -6054,6 +6057,9 @@ static void rebuild_nonce(struct work *work, uint32_t nonce)
 			break;
 		case KL_MYRIADCOIN_GROESTL:
 			myriadcoin_groestl_regenhash(work);
+			break;
+		case KL_ANIMECOIN:
+			animecoin_regenhash(work);
 			break;
 		default:
 			scrypt_regenhash(work);
