e32dcd7190bcf25bb283befa84e3e4807c46bd6b
elbandi <bandispam@gmail.com>
2014-05-07 00:02:10 +0200

    Mark pool as idle if stratum restart is failed

--- sgminer.c
+++ sgminer.c
@@ -5430,6 +5430,7 @@ static void *stratum_rthread(void *userdata)
 			if (!restart_stratum(pool)) {
 				pool_died(pool);
 				while (!restart_stratum(pool)) {
+					pool_failed(pool);
 					if (pool->removed)
 						goto out;
 					cgsleep_ms(30000);
@@ -5470,6 +5471,7 @@ static void *stratum_rthread(void *userdata)
 
 			pool_died(pool);
 			while (!restart_stratum(pool)) {
+				pool_failed(pool);
 				if (pool->removed)
 					goto out;
 				cgsleep_ms(30000);
