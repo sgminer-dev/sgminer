56055754eb682cdf9b063fb3f4a366be28d06850
ystarnaud <ystarnaud@gmail.com>
2014-06-21 12:31:48 -0400

    Whitespace and indentation fix
    Fixed whitespace and indentation of lasybear's pull request #272.

--- algorithm/talkcoin.c
+++ algorithm/talkcoin.c
@@ -38,24 +38,24 @@
 #include "sph/sph_skein.h"
 
 /* Move init out of loop, so init once externally, and then use one single memcpy with that bigger memory block */
-typedef struct {
-    sph_blake512_context    blake1;
-    sph_groestl512_context  groestl1;
-    sph_jh512_context       jh1;
-    sph_keccak512_context   keccak1;
-    sph_skein512_context    skein1;
+typedef struct 
+{
+  sph_blake512_context    blake1;
+  sph_groestl512_context  groestl1;
+  sph_jh512_context       jh1;
+  sph_keccak512_context   keccak1;
+  sph_skein512_context    skein1;
 } Xhash_context_holder;
 
 Xhash_context_holder base_contexts;
 
-
 void init_Nhash_contexts()
 {
-    sph_blake512_init(&base_contexts.blake1);
-    sph_groestl512_init(&base_contexts.groestl1);
-    sph_jh512_init(&base_contexts.jh1);
-    sph_keccak512_init(&base_contexts.keccak1);
-    sph_skein512_init(&base_contexts.skein1);
+  sph_blake512_init(&base_contexts.blake1);
+  sph_groestl512_init(&base_contexts.groestl1);
+  sph_jh512_init(&base_contexts.jh1);
+  sph_keccak512_init(&base_contexts.keccak1);
+  sph_skein512_init(&base_contexts.skein1);
 }
 
 /*
@@ -65,39 +65,39 @@ void init_Nhash_contexts()
 static inline void
 be32enc_vect(uint32_t *dst, const uint32_t *src, uint32_t len)
 {
-        uint32_t i;
+  uint32_t i;
 
-        for (i = 0; i < len; i++)
-                dst[i] = htobe32(src[i]);
+  for (i = 0; i < len; i++)
+    dst[i] = htobe32(src[i]);
 }
 
 
 inline void talkhash(void *state, const void *input)
 {
-    init_Nhash_contexts();
+  init_Nhash_contexts();
 
-    Xhash_context_holder ctx;
+  Xhash_context_holder ctx;
 
-    uint32_t hashA[16], hashB[16];
-    //blake-bmw-groestl-sken-jh-meccak-luffa-cubehash-shivite-simd-echo
-    memcpy(&ctx, &base_contexts, sizeof(base_contexts));
+  uint32_t hashA[16], hashB[16];
+  //blake-bmw-groestl-sken-jh-meccak-luffa-cubehash-shivite-simd-echo
+  memcpy(&ctx, &base_contexts, sizeof(base_contexts));
 
-    sph_blake512 (&ctx.blake1, input, 80);
-    sph_blake512_close (&ctx.blake1, hashA);
+  sph_blake512 (&ctx.blake1, input, 80);
+  sph_blake512_close (&ctx.blake1, hashA);
 
-    sph_groestl512 (&ctx.groestl1, hashA, 64);
-    sph_groestl512_close(&ctx.groestl1, hashB);
+  sph_groestl512 (&ctx.groestl1, hashA, 64);
+  sph_groestl512_close(&ctx.groestl1, hashB);
 
-    sph_jh512 (&ctx.jh1, hashB, 64);
-    sph_jh512_close(&ctx.jh1, hashA);
+  sph_jh512 (&ctx.jh1, hashB, 64);
+  sph_jh512_close(&ctx.jh1, hashA);
 
-    sph_keccak512 (&ctx.keccak1, hashA, 64);
-    sph_keccak512_close(&ctx.keccak1, hashB);
+  sph_keccak512 (&ctx.keccak1, hashA, 64);
+  sph_keccak512_close(&ctx.keccak1, hashB);
 
-    sph_skein512 (&ctx.skein1, hashB, 64);
-    sph_skein512_close(&ctx.skein1, hashA);
+  sph_skein512 (&ctx.skein1, hashB, 64);
+  sph_skein512_close(&ctx.skein1, hashA);
 
-    memcpy(state, hashA, 32);
+  memcpy(state, hashA, 32);
 }
 
 static const uint32_t diff1targ = 0x0000ffff;
@@ -114,26 +114,29 @@ int talkcoin_test(unsigned char *pdata, const unsigned char *ptarget, uint32_t n
 
 	tmp_hash7 = be32toh(ohash[7]);
 
-	applog(LOG_DEBUG, "htarget %08lx diff1 %08lx hash %08lx",
-				(long unsigned int)Htarg,
-				(long unsigned int)diff1targ,
-				(long unsigned int)tmp_hash7);
+	applog(LOG_DEBUG, "htarget %08lx diff1 %08lx hash %08lx", 
+    (long unsigned int)Htarg,
+		(long unsigned int)diff1targ,
+		(long unsigned int)tmp_hash7);
+    
 	if (tmp_hash7 > diff1targ)
 		return -1;
+    
 	if (tmp_hash7 > Htarg)
 		return 0;
+    
 	return 1;
 }
 
 void talkcoin_regenhash(struct work *work)
 {
-        uint32_t data[20];
-        uint32_t *nonce = (uint32_t *)(work->data + 76);
-        uint32_t *ohash = (uint32_t *)(work->hash);
+  uint32_t data[20];
+  uint32_t *nonce = (uint32_t *)(work->data + 76);
+  uint32_t *ohash = (uint32_t *)(work->hash);
 
-        be32enc_vect(data, (const uint32_t *)work->data, 19);
-        data[19] = htobe32(*nonce);
-        talkhash(ohash, data);
+  be32enc_vect(data, (const uint32_t *)work->data, 19);
+  data[19] = htobe32(*nonce);
+  talkhash(ohash, data);
 }
 
 bool scanhash_talkcoin(struct thr_info *thr, const unsigned char __maybe_unused *pmidstate,
@@ -149,28 +152,30 @@ bool scanhash_talkcoin(struct thr_info *thr, const unsigned char __maybe_unused 
 
 	be32enc_vect(data, (const uint32_t *)pdata, 19);
 
-	while(1) {
-		uint32_t ostate[8];
+	while(1) 
+  {
+    uint32_t ostate[8];
 
-		*nonce = ++n;
-		data[19] = (n);
-		talkhash(ostate, data);
-		tmp_hash7 = (ostate[7]);
+    *nonce = ++n;
+    data[19] = (n);
+    talkhash(ostate, data);
+    tmp_hash7 = (ostate[7]);
 
-		applog(LOG_INFO, "data7 %08lx",
-					(long unsigned int)data[7]);
+    applog(LOG_INFO, "data7 %08lx",	(long unsigned int)data[7]);
 
-		if (unlikely(tmp_hash7 <= Htarg)) {
-			((uint32_t *)pdata)[19] = htobe32(n);
-			*last_nonce = n;
-			ret = true;
-			break;
-		}
+    if (unlikely(tmp_hash7 <= Htarg)) 
+    {
+      ((uint32_t *)pdata)[19] = htobe32(n);
+      *last_nonce = n;
+      ret = true;
+      break;
+    }
 
-		if (unlikely((n >= max_nonce) || thr->work_restart)) {
-			*last_nonce = n;
-			break;
-		}
+    if (unlikely((n >= max_nonce) || thr->work_restart)) 
+    {
+      *last_nonce = n;
+      break;
+    }
 	}
 
 	return ret;
--- algorithm/talkcoin.h
+++ algorithm/talkcoin.h
@@ -3,8 +3,7 @@
 
 #include "miner.h"
 
-extern int talkcoin_test(unsigned char *pdata, const unsigned char *ptarget,
-			uint32_t nonce);
+extern int talkcoin_test(unsigned char *pdata, const unsigned char *ptarget, uint32_t nonce);
 extern void talkcoin_regenhash(struct work *work);
 
 #endif /* TALKCOIN_H */
