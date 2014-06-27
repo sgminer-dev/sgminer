10fcbd3302db51749da5c84f58b27826c2db7256
Noel Maersk <veox@wemakethings.net>
2014-06-27 18:24:28 +0300

    tools: typo in update_changelog.sh.

--- tools/update_changelog.sh
+++ tools/update_changelog.sh
@@ -4,6 +4,6 @@ FILE="$(dirname "$0")/../ChangeLog"
 UPDATECMD="git log --oneline --decorate"
 
 echo "Updated on `date`." > $FILE
-echo "If working with git, use \`$UDATECMD\` for latest change log." >> $FILE
+echo "If working with git, use \`$UPDATECMD\` for latest change log." >> $FILE
 echo "------------------------------------------------------------------------" >> $FILE
 $UPDATECMD >> $FILE
