8b44007901c6440a7b727e838f46269547e5a14e
troky <troky2001@yahoo.com>
2014-07-09 10:39:36 +0200

    Fixed compatilibilty issues on MSVS.

--- winbuild/dist/include/unistd.h
+++ winbuild/dist/include/unistd.h
@@ -1,1 +1,11 @@
 #pragma once
+
+// POSIX open,read,write... deprecated as of VC++ 2005.
+// Use ISO conformant _open,_read,_write instead.
+#define open _open
+#define write _write
+#define close _close
+#define read _read
+#define snprintf _snprintf
+#define dup _dup
+#define dup2 _dup2
