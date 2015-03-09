f366619269e65c6badbbb3120fe774d0e5fa87db
troky <troky2001@yahoo.com>
2015-03-09 11:05:42 +0100

    Version bump to 5.1.1

--- winbuild/dist/include/config.h
+++ winbuild/dist/include/config.h
@@ -67,11 +67,11 @@
 
 #endif
 
-#define VERSION "v5.1.0"
+#define VERSION "v5.1.1"
 #define PACKAGE_NAME "sgminer"
 #define PACKAGE_TARNAME "sgminer"
-#define PACKAGE_VERSION "5.1.0"
-#define PACKAGE_STRING "sgminer 5.1.0"
+#define PACKAGE_VERSION "5.1.1"
+#define PACKAGE_STRING "sgminer 5.1.1"
 #define PACKAGE "sgminer"
 
 #define SGMINER_PREFIX ""
